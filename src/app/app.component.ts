e8bd1b8e067891ba1d856c051d9162808278dafc
unknown <manoth.4@gmail.com>
2018-01-30 14:08:29 +0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  loginPage: string;
+  regitPage: string;
+
+  token: boolean = false;
+
+  ngOnInit() {
+    this.loginPage = this.loginPath('signin');
+    this.regitPage = this.loginPath('signup');
+  }
+
+  loginPath(onPath: string) {
+    let followup = this.fullPath();
+    let client = this.utoa('cpho#005');
+    return `http://203.157.182.15/accountsv2/${onPath}/?followup=${followup}&cli=${client}`;
+  }
+
+  fullPath() {
+    return window.location.href;
+  }
+
+  // ucs-2 string to base64 encoded ascii
+  utoa(str) {
+    return window.btoa(str).replace('=','');
+  }
+  // base64 encoded ascii to ucs-2 string
+  atou(str) {
+    return window.atob(str.replace('=',''));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// Rounting
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'dash1', pathMatch: 'full' },
+  { path: 'dash1', component: DashboardComponent},
+  { path: 'dash2', component: Dashboard2Component, data: { a: 'aaa', b: 'bbb' } },
+  { path: 'dash3', component: Dashboard3Component, data: { a: '111', b: '222' } },
+  { path: '**', redirectTo: 'dash1', pathMatch: 'full' }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DashboardComponent,
+    FooterComponent,
+    Dashboard2Component,
+    Dashboard3Component
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { useHash: false })
   ],
   providers: [],
   bootstrap: [AppComponent]
